8dd4dd18f202a0a79dd07122260ce53b8e36a38b
Tiago Henrique Macena <t.hmacena@hotmail.com>
2022-01-27 19:51:44 -0300

    Repositório curso de lógica de programação com JavaScript FIAP- SHIFT

---
+++ Hello_World/Js/script.js
@@ -0,0 +1,5 @@
+let nome=prompt("Digite o seu nome: ");
+alert(`Olá ${nome}, seja bem vindo a imersão string`);
+document.getElementById("saida").innerHTML=
+`Olá ${nome}, seja bem vindo a imersão string`
+alert ("Hello World!");
